50cce6af1b3d1628fb0f1cb4be98f88e8441c45c
BlueSkyCaps <linsongxian@outlook.com>
2023-04-07 23:06:11 +0800

    测试压缩gif动图：每帧jpg压缩

--- core/test/test_all_gif_compress.go
+++ core/test/test_all_gif_compress.go
@@ -10,6 +10,67 @@ import (
 )
 
 func GifAllTest() {
+	//inputGifBoss := &gif.GIF{}
+	// 打开原始GIF文件
+	f, err := os.Open("C:\\Users\\BlueSkyCarry\\Desktop\\2S60X}}KL%HZ`H8$MX8MIRA.gif")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			panic(err.Error())
+		}
+	}(f)
+
+	// 解码GIF文件
+	gifImg, err := gif.DecodeAll(f)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// 保存每一帧图像为单独的gif文件
+	for i, frame := range gifImg.Image {
+		_ = i
+		// 每帧图像
+		//inputGifBoss.Image = append(inputGifBoss.Image, frame)
+		//inputGifBoss.Delay = append(inputGifBoss.Delay, 0)
+		//inputGifBoss.Config = image.Config{ColorModel: inputGifBoss.Config.ColorModel, Width: inputGifBoss.Image[0].Bounds().Dx(),
+		//	Height: inputGifBoss.Image[0].Bounds().Dy()}
+		newFrameFs, err := os.Create("static/test/img/frames/" + strconv.Itoa(i) + ".gif")
+		if err != nil {
+			panic(err.Error())
+		}
+
+		//if err := gif.EncodeAll(newFrameFs, inputGifBoss); err != nil {
+		//	panic(err.Error())
+		//}
+		imageFrame := image.Image(frame)
+		if err := gif.Encode(newFrameFs, imageFrame, nil); err != nil {
+			panic(err.Error())
+		}
+		err = newFrameFs.Close()
+		if err != nil {
+			panic(err.Error())
+		}
+		//inputGifBoss = &gif.GIF{}
+		openedGif, err := imaging.Open("static/test/img/frames/" + strconv.Itoa(i) + ".gif")
+		if err != nil {
+			panic(err.Error())
+		}
+		err = imaging.Save(openedGif, "static/test/img/frames/"+strconv.Itoa(i)+".jpg", imaging.JPEGQuality(70))
+		if err != nil {
+			return
+		}
+		err = os.Remove("static/test/img/frames/" + strconv.Itoa(i) + ".gif")
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+}
+
+// GifReduceFrameTest 减少帧数的方式压缩源gif
+func GifReduceFrameTest() {
 	inputGifBoss := &gif.GIF{}
 	// 打开原始GIF文件
 	f, err := os.Open("C:\\Users\\BlueSkyCarry\\Desktop\\微信图片_20230405212507.gif")
